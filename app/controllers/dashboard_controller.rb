991fb6c1bafc5050a6445e0710c257741370232a
Wyatt Greene <greenewm@yahoo.com>
2008-10-18 11:31:23 -0400

    Added index to dashboard controller.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,6 @@
 class DashboardController < ApplicationController
 
+  def index
+  end
+
 end
--- test/functional/dashboard_controller_test.rb
+++ test/functional/dashboard_controller_test.rb
@@ -4,9 +4,17 @@ class DashboardControllerTest < ActionController::TestCase
   def setup
   end
   
-  context "viewing index" do
+  context "viewing homepage" do
     should_route :get, '/', :controller => :dashboard, :action => :index
   end
 
+  context "viewing dashboard" do
+    setup do
+      assert_nothing_raised { get :index }
+    end
   
+    should_respond_with :success
+    should_render_template :index
+  end
+      
 end
