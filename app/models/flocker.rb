a60207e947446401d60aede6732c42800688afdf
Josh Nichols <josh@technicalpickles.com>
2008-10-18 10:49:45 -0400

    Starting work to verify flocker.

--- app/models/flocker.rb
+++ app/models/flocker.rb
@@ -1,4 +1,8 @@
 class Flocker < ActiveRecord::Base
   has_and_belongs_to_many :flocks
   validates_uniqueness_of :twitter_username
+  
+  def unverified?
+    true
+  end
 end
--- test/unit/flocker_test.rb
+++ test/unit/flocker_test.rb
@@ -1,11 +1,25 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class FlockerTest < ActiveSupport::TestCase
-  def setup
-    @flocker = Factory(:flocker)
-  end
-  
+    
   should_have_db_column :twitter_username
   should_have_and_belong_to_many :flocks
-  should_require_unique_attributes :twitter_username
+  
+  context "An existing flock" do
+    setup do
+      @flocker = Factory(:flocker)
+    end
+
+    should_require_unique_attributes :twitter_username
+  end
+  
+  context "a new flock" do
+    setup do
+      @flock = Factory.build(:flocker, :twitter_username => 'techpickles')
+    end
+
+    should "be unverified" do
+      assert @flock.unverified?
+    end
+  end
 end
