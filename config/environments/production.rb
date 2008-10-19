0519e1084bd68e002abba5390eea8bae9a77926c
Josh Nichols <josh@technicalpickles.com>
2008-10-18 22:52:32 -0400

    Use starling in production. Updated .gitignore.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,3 +4,5 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 
 config.action_view.cache_template_loading            = true
+
+Workling::Remote.dispatcher = Workling::Remote::Runners::StarlingRunner.new
