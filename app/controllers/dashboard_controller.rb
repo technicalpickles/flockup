9f69b9a4f65ecbb9ebee7afc9e217e7a9571b21a
Josh Nichols <josh@technicalpickles.com>
2008-10-19 18:11:14 -0400

    Search tab. Tweaks to search page.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,9 +4,15 @@ class DashboardController < ApplicationController
   end
   
   def search
-    @results = Flock.find(:all, :conditions => ['name LIKE ?', "%#{params[:search].downcase}%"]) + Flocker.find(:all, :conditions => ['twitter_username LIKE ?', "%#{params[:search].downcase}%"])
-    if @results.size == 1
-      redirect_to @results.first
+    @search_terms = params[:search]
+
+    unless @search_terms.blank?
+      @search_terms = @search_terms.downcase
+      # gah, should be in a model somewhere
+      @results = Flock.find(:all, :conditions => ['name LIKE ?', "%#{@search_terms}%"]) + Flocker.find(:all, :conditions => ['twitter_username LIKE ?', "%#{@search_terms}%"])
+      if @results.size == 1
+        redirect_to @results.first
+      end
     end
   end
 
