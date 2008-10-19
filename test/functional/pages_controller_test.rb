b6ebadfe4f447eecb15c793e8a887e0b62c9ba2d
Josh Nichols <josh@technicalpickles.com>
2008-10-19 11:40:42 -0400

    Fixed tests

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -1,9 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  def setup
-  end
-  
   context "viewing about" do
     setup do
       assert_nothing_raised { get :about }
@@ -11,21 +8,4 @@ class PagesControllerTest < ActionController::TestCase
     should_respond_with :success
     should_render_template :about
   end
-
-  context "viewing terms of service" do
-    setup do
-      assert_nothing_raised { get :terms_of_service }
-    end
-    should_respond_with :success
-    should_render_template :terms_of_service
-  end
-
-  context "viewing contact" do
-    setup do
-      assert_nothing_raised { get :contact }
-    end
-    should_respond_with :success
-    should_render_template :contact
-  end
-
 end
