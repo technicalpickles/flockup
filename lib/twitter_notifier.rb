77a80d362d93c9b944e2b0e0a7e75c44ebb46e06
Josh Nichols <josh@technicalpickles.com>
2008-10-19 01:40:49 -0400

    fix logger for notifier.

--- lib/twitter_notifier.rb
+++ lib/twitter_notifier.rb
@@ -1,7 +1,7 @@
 module TwitterNotifier
   def notify_on_twitter(message)
     message_with_to = "@#{self.twitter_username} #{message}"
-    log.info "About to twitter: #{message_with_to}"
+    logger.info "About to twitter: #{message_with_to}"
     twitter.post(message_with_to)
   end
   
