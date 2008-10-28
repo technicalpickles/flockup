4d845fb65d9b838a7ec50c754c8426f2a9b98274
Josh Nichols <josh@technicalpickles.com>
2008-10-27 20:48:19 -0400

    Remove extra formatting from flockers_controller#create.

--- app/controllers/flockers_controller.rb
+++ app/controllers/flockers_controller.rb
@@ -18,40 +18,29 @@ class FlockersController < ApplicationController
   def create
     self.resource = new_resource
     
-    respond_to do |format|
-      if !new_resource.new_record?
-        format.html do
-          if enclosing_resource
-            if enclosing_resource.flocker_ids.include?(resource.id)
-              flash[:notice] = "#{resource.twitter_username} already in the #{enclosing_resource.name} flock"
-            else
-              flash[:notice] = "#{resource.twitter_username} added to the #{enclosing_resource.name} flock"
-              enclosing_resource.flockers << resource
-            end
-            redirect_to enclosing_resource_url
-          else
-            redirect_to resource_url
-          end
+    if !new_resource.new_record?
+      if enclosing_resource
+        if enclosing_resource.flocker_ids.include?(resource.id)
+          flash[:notice] = "#{resource.twitter_username} already in the #{enclosing_resource.name} flock"
+        else
+          flash[:notice] = "#{resource.twitter_username} added to the #{enclosing_resource.name} flock"
+          enclosing_resource.flockers << resource
         end
-      elsif resource.save
-        format.html do
-
-          if enclosing_resource
-            flash[:notice] = "#{resource.twitter_username} was added to the #{enclosing_resource.name} flock"
-            enclosing_resource.flockers << resource
-            redirect_to enclosing_resource_url
-          else
-            flash[:notice] = "#{resource.twitter_username} was successfully created."
-            redirect_to resource_url
-          end
-        end
-        format.js
-        format.xml  { render :xml => resource, :status => :created, :location => resource_url }
+        redirect_to enclosing_resource_url
       else
-        format.html { render :action => "new" }
-        format.js   { render :action => "new" }
-        format.xml  { render :xml => resource.errors, :status => :unprocessable_entity }
+        redirect_to resource_url
       end
+    elsif resource.save
+      if enclosing_resource
+        flash[:notice] = "#{resource.twitter_username} was added to the #{enclosing_resource.name} flock"
+        enclosing_resource.flockers << resource
+        redirect_to enclosing_resource_url
+      else
+        flash[:notice] = "#{resource.twitter_username} was successfully created."
+        redirect_to resource_url
+      end
+    else
+      render :action => "new" 
     end
   end
   
