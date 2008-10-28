f4bea425629646a751b46cbf8590a2e18eb58c06
Josh Nichols <josh@technicalpickles.com>
2008-10-27 20:54:39 -0400

    Removed workers.

--- app/workers/flocker_worker.rb
+++
@@ -1,8 +0,0 @@
-class FlockerWorker < Workling::Base
-  def verify(options)
-    flocker = Flocker.find(options[:id])
-    logger.info("#{flocker.twitter_username}: about to verify (currently #{flocker.status})")
-    flocker.verify!
-    logger.info("#{flocker.twitter_username}: updated status to #{flocker.status}")
-  end
-end
