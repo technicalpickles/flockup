438242edce7bc0b616fb1e528369f3cffebc54b8
Josh Nichols <josh@technicalpickles.com>
2009-01-12 21:52:05 -0500

    Removed notification stuff.

--- app/models/flock.rb
+++ app/models/flock.rb
@@ -4,7 +4,7 @@ class Flock < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_format_of :name, :with => /^[a-z\d_]+$/
   
-  has_and_belongs_to_many :flockers, :after_add => :notify_added, :after_remove => :notify_removed
+  has_and_belongs_to_many :flockers
   
   attr_accessible :name, :description
   
@@ -14,19 +14,4 @@ class Flock < ActiveRecord::Base
   @@per_row = 5
   cattr_accessor :per_page
   @@per_page = @@per_row*7
-  
-  def notify_added(flocker)
-    unless flocker.twitter_username.blank? || !flocker.verified_twitter_username?
-      @notice = Notification.create(:flocker => flocker, :text => "flockup.com provides user-generated groups for twitter and someone added you: #{APP_URL}/flockers/#{self.id}")
-      @notice.push('notify_flocker')
-    end
-  end
-  
-  def notify_removed(flocker)
-    unless flocker.twitter_username.blank? || !flocker.verified_twitter_username?
-      @notice = Notification.create(:flocker => flock, :text => "you've been added to the #{flock.name} flock #{APP_URL}/flocks/#{flock.id}")
-      @notice.push('notify_flocker')
-    end
-  end
-  
 end
