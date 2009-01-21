701fed4694f29360e786b2de4f0dc16fb125e39d
Josh Nichols <josh@technicalpickles.com>
2009-01-21 17:07:37 -0500

    Added 'date' time format, and use it for announcements.

---
+++ config/initializers/date_format.rb
@@ -0,0 +1,6 @@
+my_formats = {
+  :date => '%m/%d/%Y',
+}
+
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(my_formats)
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(my_formats)
