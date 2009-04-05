adbe234e569e174e1dc896aff083757bfbeadb4e
Josh Nichols <josh@technicalpickles.com>
2009-04-04 21:01:56 -0400

    Removed outdated environment configuration.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,6 @@ config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 
-config.action_view.cache_template_loading            = true
+#config.action_view.cache_template_loading            = true
 
 APP_URL = "http://flockup.com"
